7a31199ba1be616a729145681aa7622b9528d35a
Hazzoz <hwils2019@gmail.com>
2024-10-06 17:29:51 +1100

    Proper Testing for Basic SIR

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,22 @@ using SIR_model
 using Test
 
 @testset "BasicSIR" begin
-    @test solve_SIR(5000,1,0,60,BasicSIR(0.3,0.5)) == 1
+    # Solve the SIR model
+    sol = solve_SIR(5000, 1, 0, 60, BasicSIR(0.3, 0.1))
+
+    # Get the initial total population
+    initial_total = sum(sol[1])
+
+    # Check that the total population remains the same at every time step
+    for i in sol
+        @test sum(i) ≈ initial_total
+    end
 end
 
 @testset "SIRForceOfInfection" begin
-    @test solve_SIR(5000,1,0,60,SIRForceOfInfection(0.3,0.5,10)) == 1
+    #@test solve_SIR(5000,1,0,60,SIRForceOfInfection(0.3,0.5,10)) == 1
 end
 
 @testset "SIRHerdImmunity" begin
-    @test solve_SIR(5000,1,0,60,SIRHerdImmunity(0.3,0.5,10,0.8)) == 1
+    #@test solve_SIR(5000,1,0,60,SIRHerdImmunity(0.3,0.5,10,0.8)) == 1
 end
