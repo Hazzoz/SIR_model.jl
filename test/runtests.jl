cf97842851b82b9444a0ae7f63792cb765e17aec
Hazzoz <hwils2019@gmail.com>
2024-10-08 09:02:11 +1100

    More Tests for Herd

--- test/runtests.jl
+++ test/runtests.jl
@@ -212,6 +212,7 @@ end
 end
 
 @testset "SIRHerdImmunity" begin
+    ##################### Normal Function
     # Solve the SIR model
     sol, lambda = solve_SIR(5000,1,0,60,SIRHerdImmunity(0.3,0.5,10,[]))
 
@@ -230,7 +231,73 @@ end
             check = 1
         end
         if check != 0
-            @test i[1] == limit
+            @test i[1] ≈ limit atol = 0.01
+        end
+    end
+    ##################### High Beta
+    # Solve the SIR model
+    sol, lambda = solve_SIR(5000,1,0,60,SIRHerdImmunity(0.9,0.5,10,[]))
+
+    # Calculate the herd immunity threshold
+    R0 = 0.3*10/0.5
+    pc = 1 - 1/R0
+
+    # Loop through and find the point where the recovered population exceeds 
+    # the threshold and then check that each subsequent point doesn't change
+    # (as beta is 0)
+    check = 0
+    limit = 0
+    for i in sol.u
+        if sum(i)*pc <= i[3] && check == 0
+            limit = i[1]
+            check = 1
+        end
+        if check != 0
+            @test i[1] ≈ limit atol = 0.01
+        end
+    end
+    ##################### High Gamma
+    # Solve the SIR model
+    sol, lambda = solve_SIR(5000,1,0,60,SIRHerdImmunity(0.3,0.9,10,[]))
+
+    # Calculate the herd immunity threshold
+    R0 = 0.3*10/0.5
+    pc = 1 - 1/R0
+
+    # Loop through and find the point where the recovered population exceeds 
+    # the threshold and then check that each subsequent point doesn't change
+    # (as beta is 0)
+    check = 0
+    limit = 0
+    for i in sol.u
+        if sum(i)*pc <= i[3] && check == 0
+            limit = i[1]
+            check = 1
+        end
+        if check != 0
+            @test i[1] ≈ limit atol = 0.01
+        end
+    end
+    ##################### No Infected
+    # Solve the SIR model
+    sol, lambda = solve_SIR(5000,0,0,60,SIRHerdImmunity(0.3,0.5,10,[]))
+
+    # Calculate the herd immunity threshold
+    R0 = 0.3*10/0.5
+    pc = 1 - 1/R0
+
+    # Loop through and find the point where the recovered population exceeds 
+    # the threshold and then check that each subsequent point doesn't change
+    # (as beta is 0)
+    check = 0
+    limit = 0
+    for i in sol.u
+        if sum(i)*pc <= i[3] && check == 0
+            limit = i[1]
+            check = 1
+        end
+        if check != 0
+            @test i[1] ≈ limit atol = 0.01
         end
     end
 end
